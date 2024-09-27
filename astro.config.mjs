76066cc197e1875fbbe380094dd7b4b0358b6b77
EugenAchtzehn <eugene1826@gmail.com>
2024-09-27 21:39:03 +0800

    [Change] set path back, after setting up DNS for joysplanting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eugenachtzehn.github.io/',
-  base: 'joysplanting',
+  site: 'https://new.joysplanting.com',
+  // base: 'joysplanting',
 });
