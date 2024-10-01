8e2714ac6475d2865c466ae85bdb14c86fceedeb
EugenAchtzehn <eugene1826@gmail.com>
2024-10-01 18:48:21 +0800

    [Add/Fixbug] major, run npm audit and partytown lib for performance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://new.joysplanting.com'
+  site: 'https://new.joysplanting.com',
   // base: 'joysplanting',
-  ,
-  integrations: [sitemap()]
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
