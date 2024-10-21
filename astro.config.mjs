a2860821e949e2d21586666b01cdd64613167334
EugenAchtzehn <eugene1826@gmail.com>
2024-10-21 16:27:02 +0800

    [Add] integration with Vue.js, a counter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@ import { defineConfig } from 'astro/config';
 
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new.joysplanting.com',
   // base: 'joysplanting',
-  integrations: [sitemap(), partytown({
-    config: {
-      forward: ['dataLayer.push'],
-    },
-  }), vue()],
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    vue(),
+  ],
 });
---
+++ src/components/AppVuePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container py-3">
+    <div class="row g-3">
+      <div class="col-md-4">
+        <div class="content-box p-3">
+          <div class="input-group mb-3">
+            <button class="btn btn-primary" type="button" @click="increment">Increment</button>
+          </div>
+          <div>Counter: {{ counter }}</div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="content-box"></div>
+      </div>
+      <div class="col-md-4">
+        <div class="content-box"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  const counter = ref(0);
+
+  const increment = () => {
+    counter.value++;
+  };
+</script>
